33e3c2f3fee3d2b9954a2006a66de4e54cf68926
akriaueno <akriaueno@outlook.com>
2021-03-05 20:33:21 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+shell_checker () {
+  ppid=$(ps -o ppid -p $$ | tail -n 1)
+  ps -p $ppid | tail -n 1 | awk '
+  match($0,/bash|zsh|fish|tcsh/) {
+    print substr($0,RSTART,RLENGTH)
+  }'
+}
+
+shell=$(shell_checker)
+echo $shell
